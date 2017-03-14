3f19d13f16ae3a30bc8cf14aca253832d746577e
rizaldi <rizaldichozzone@gmail.com>
2017-03-14 10:59:06 +0700

    update logistic_demo/public/

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-    $("#link a").on('click', function(event) {
-        event.preventDefault();
-        var targetUrl = $(this).attr('href'),
-        targetTitle = $(this).attr('title');
-        if ( $(this).attr('href') != '#' ) {
-            window.history.pushState({url: "" + targetUrl + ""}, targetTitle, targetUrl);
-            $('#ajax-content').load($(this).attr('href'));
-            window.location.reload();
-        }
-    });    
-});
+	// $(document).ready(function() {
+	//     $("#link a").on('click', function(event) {
+	//         event.preventDefault();
+	//         var targetUrl = $(this).attr('href'),
+	//         targetTitle = $(this).attr('title');
+	//         if ( $(this).attr('href') != '#' ) {
+	//             window.history.pushState({url: "" + targetUrl + ""}, targetTitle, targetUrl);
+	//             $('#ajax-content').load($(this).attr('href'));
+	//             location.reload();
+	//         }
+	//     });    
+	// });
